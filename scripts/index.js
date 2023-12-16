63983c0ef443cfb2b7755dcef3f120d02bf23c29
Jija <novikovwarlock@yandex.ru>
2023-12-16 21:19:28 +0300

    на тесты

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,31 @@
 
 // @todo: Функция удаления карточки
 
+
 // @todo: Вывести карточки на страницу
+
+
+const container = document.querySelector('.content');
+const cardContainer = container.querySelector('.places__list');
+
+function createCard(item, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDelete = cardElement.querySelector('.card__delete-button')
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    deleteCard(cardElement);
+    return cardElement;
+}
+
+function deleteCard(item) {
+    const cardDelete = item.querySelector('.card__delete-button')
+    cardDelete.addEventListener('click', function() {
+        item.remove();
+    });
+}
+
+initialCards.forEach(item => {
+    const card = createCard(item, deleteCard);
+    cardContainer.append(card);
+});
